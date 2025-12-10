48edfdb26d1abd1ed93749e6cc4d445d7ae662ff
liver-51 <202583890033@nuist.edu.cn>
2025-12-10 19:21:47 +0800

    Add files via upload

---
+++ v1.0.9/src/Store.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+
+public class Store {
+
+    private Pet[] pets;
+    private int total = 0;
+
+    public int getCapacity() {
+        return pets.length;
+    }
+
+    public int getTotalPets() {
+        return total;
+    }
+
+    public Store(int numberItems) {
+        pets = new Pet[numberItems];
+        total = 0;
+
+    }
+
+    public boolean add(Pet pet) {
+        if (total < pets.length) {
+            pets[total] = pet;
+            total++;
+            System.out.print("Added successfully!");
+            return true;
+        }
+        return false;
+    }
+
+
+    private boolean isFull() {
+        return total == pets.length;
+    }
+
+    private boolean isEmpty() {
+        return total == 0;
+    }
+}
